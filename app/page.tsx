e99933af28b89779a32650aa0f2603a82f8ce6cd
sindhu <ssindhujak69@gmail.com>
2026-02-26 22:06:25 +0530

    add

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,30 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Gallery from "./components/Gallery";
 import Footer from "./components/Footer";
-import ProtectedRoute from "./components/ProtectedRoute";
 
 export default function Home() {
   return (
-    <ProtectedRoute>
+    <>
       <Navbar />
       <Hero />
       <Gallery />
       <Footer />
-    </ProtectedRoute>
+    </>
   );
 }
+// import Navbar from "./components/Navbar";
+// import Hero from "./components/Hero";
+// import Gallery from "./components/Gallery";
+// import Footer from "./components/Footer";
+// import ProtectedRoute from "./components/ProtectedRoute";
+
+// export default function Home() {
+//   return (
+//     <ProtectedRoute>
+//       <Navbar />
+//       <Hero />
+//       <Gallery />
+//       <Footer />
+//     </ProtectedRoute>
+//   );
+// }
