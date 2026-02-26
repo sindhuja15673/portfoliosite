3eaccb0f1639716d5e66547c95af200736010e13
sindhu <ssindhujak69@gmail.com>
2026-02-26 20:55:40 +0530

    config errors

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,25 @@
 // };
 
 // export default nextConfig;
-module.exports = {
+// module.exports = {
+//   images: {
+//     domains: ["jbzhmzjiefvlqxavsokn.supabase.co"],
+//   },
+// };
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
   images: {
-    domains: ["jbzhmzjiefvlqxavsokn.supabase.co"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.supabase.co",
+      },
+    ],
   },
 };
+
+module.exports = nextConfig;
